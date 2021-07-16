8ff1330931ef8020eb77b628f5d1d20ed5765bbe
Dorian <dorian.guler2021@campus-eni.fr>
2021-07-16 18:13:22 +0200

    Commit Form Edit Profile + Password et Controller User

---
+++ src/Controller/EditUserController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\EditAccountFormType;
+use App\Form\EditPasswordFormType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+class EditUserController extends AbstractController
+{
+    /**
+     * @Route("/user/profile", name="user_profile")
+     */
+    public function profileUser(User $user): Response
+    {
+        return $this->render( 'user/profileUser.html.twig', [
+            'user' => $user,
+        ]);
+    }
+
+    /**
+     * @Route("/user/modify", name="user_modify")
+     */
+    public function editUser(Request $request): Response
+    {
+        $user = $this->getUser();
+
+        $editUserForm = $this->createForm(EditAccountFormType::class, $user);
+        $editUserForm->handleRequest($request);
+
+        $entityManager = $this->getDoctrine()->getManager();
+
+        if ($editUserForm->isSubmitted() && $editUserForm->isValid()) {
+            $entityManager->persist($user);
+            $entityManager->flush();
+            return $this->redirectToRoute("user_profile");
+        }
+
+        return $this->render('user/editUser.html.twig', [
+            'editUserForm' => $editUserForm->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/user/password", name="user_password")
+     */
+    public function editPassword(UserPasswordHasherInterface $passwordHasher, EntityManagerInterface $entityManager): Response
+    {
+        $user = $this->getUser();
+
+        $editPasswordForm = $this->createForm(EditPasswordFormType::class, $user);
+        $editPasswordForm->handleRequest();
+
+        if($editPasswordForm->isSubmitted() && $editPasswordForm->isValid()) {
+            $user->setPassword(
+                $passwordHasher->hashPassword(
+                    $user,
+                    $editPasswordForm->get('password')->getData()
+                )
+            );
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            return $this->redirectToRoute("user_profile");
+        }
+        return $this->render('user/editPassword.html.twig', [
+            'editPasswordForm' => $editPasswordForm->createView(),
+        ]);
+    }
+}
---
+++ src/Form/EditAccountFormType.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class EditAccountFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('lastname', TextType::class, [
+                'label' => 'Nom de famille :',
+                'required' => false,
+            ])
+            ->add('firstname', TextType::class, [
+                'label' => 'Prénom :',
+                'required' => false,
+            ])
+            ->add('phone', TextType::class, [
+                'label' => 'Numéro de téléphone :',
+                'required' => false,
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Email :',
+                'required' => false,
+            ])
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'mapped' => false,
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Entrez votre mot de passe',
+                    ]),
+                    new Length([
+                        'min' => 6,
+                        'minMessage' => 'Votre mot de passe doit comporter au moins {{ limit }} caractères',
+                        'max' => 4096,
+                    ]),
+                ],
+                'invalid_message' => 'Mot de passe incorrect',
+                'required' => true,
+                'first_options' => ['label' => 'Mot de passe'],
+                'second_options' => ['label' => 'Répéter mot de passe'],
+            ])
+            ->add('userName',TextType::class, [
+                'label' => 'Pseudo :',
+                'required' => false,
+            ])
+            ->add('campus', EntityType::class, [
+                'label' => 'Campus :',
+                'class' => 'App\Entity\Campus',
+                'choice_label' => 'name',
+                'required' => false,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
