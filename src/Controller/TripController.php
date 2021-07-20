e8b9b37ba14813d657bad0dcf816971947074c7c
jessylfoh <82113553+jessylfoh@users.noreply.github.com>
2021-07-20 10:09:00 +0200

    Add files via upload

--- src/Controller/TripController.php
+++ src/Controller/TripController.php
@@ -1,128 +1,151 @@
 <?php
+
 namespace App\Controller;
-use App\Entity\State;
+
 use App\Entity\Trip;
+use App\Form\TripDeleteType;
 use App\Form\TripType;
+use App\Repository\PlaceRepository;
 use App\Repository\TripRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use function Sodium\add;
 
 /**
  * @Route("/trip", name="trip_")
  */
+
 class TripController extends AbstractController
 {
     /**
      * @Route("/create", name="create")
      */
+
     public function create(
         Request $request,
-        EntityManagerInterface $entityManager
+        EntityManagerInterface $entityManager,
+        PlaceRepository $placeRepository
     ): Response
     {
         //afficher le formulaire
         $trip = new Trip();
         //$trip->setState('créée');
-        //$trip->setCampus();
-        //$trip->setOrganizer();
+        //$trip->setCampus(app.user->getCampus()->getId());
+        //$trip->setOrganizer(app.user->getId());
         $tripForm = $this->createForm(TripType::class, $trip);
+
+
         //insérer les données dans l'instance
         $tripForm->handleRequest($request);
+
+
         //traiter le formulaire
         if ($tripForm->isSubmitted() && $tripForm->isValid()) {
+
             $entityManager->persist($trip);
             $entityManager->flush();
+
             //afficher un message flash
             $this->addFlash('success', 'Votre sortie a bien été créee');
         }
-            return $this->render('trip/create.html.twig', [
-                'tripForm'=>$tripForm->createView(),
-            ]);
-        }
+        return $this->render('trip/create.html.twig', [
+            'tripForm'=>$tripForm->createView(),
+            'placeDisplay' =>$placeRepository->displayPlace(1)
+        ]);
+    }
+
     /**
      * @Route("/display/{id}", name="display")
      */
+
     public function display(int $id, TripRepository $tripRepository): Response
     {
-            $trip = new Trip();
-            if (!$trip) {
-                throw $this->createNotFoundException('La sortie n\'existe pas');
-            }
-            return $this->render('trip/display.html.twig', [
-                "tripDisplay" => $tripRepository->find($id)]);
+
+        $trip=$tripRepository->find($id);
+
+        if (!$trip) {
+            throw $this->createNotFoundException('La sortie n\'existe pas');
         }
-    /**
-     * @Route("/delete", name="delete")
-     */
-    public function delete(Trip  $trip, EntityManagerInterface $entityManager) {
-            $entityManager->remove($trip);
-            $entityManager->flush();
-            return $this->redirectToRoute('main_home');
+        foreach ($trip->getParticipant() as $participants) {
+           $participants->getUserName();
+           $participants->getFirstName();
+            $participants->getLastname();
         }
+        return $this->render('trip/display.html.twig', [
+            'controller_name' => 'TripController',
+            'tripDisplay' =>$tripRepository->displayTrip($id),
+            'listParticipants'=>$trip->getParticipant() ]);
+    }
+
     /**
-     * @Route("/edit", name="edit")
+     * @Route("/delete/{id}", name="delete")
      */
-    public function edit( TripRepository $tripRepository ) {
-       $trip = new Trip();
-        $tripForm = $this->createForm(TripType::class, $trip);
-//        $trip=$tripRepository->find(1);
+
+    public function delete(
+        int $id,
+        Request $request,
+        TripRepository $tripRepository,
+        EntityManagerInterface $entityManager)
+    {
+        $trip = new Trip();
+
+        if (!$trip) {
+            throw $this->createNotFoundException('La sortie n\'existe pas');
+        }
+        $tripDeleteForm = $this->createForm(TripDeleteType::class, $trip);
+
+        //insérer les données dans l'instance
+        $tripDeleteForm->handleRequest($request);
+
+
+        //traiter le formulaire
+        if ($tripDeleteForm->isSubmitted() && $tripDeleteForm->isValid()) {
+
+//            $entityManager->persist($trip);
+//            $entityManager->flush();
+
+            //afficher un message flash
+            $this->addFlash('success', 'Votre sortie a bien été annulée');
+        }
+
+        return $this->render('trip/delete.html.twig', [
+            'controller_name' => 'TripController',
+            'tripDeleteForm'=>$tripDeleteForm->createView(),
+            'tripDisplay' =>$tripRepository->displayTrip($id),
 //
-//        if(!$trip){
-//            throw $this->createNotFoundException('La sortie n\'existe pas');
-//        }
-        return $this->render('trip/edit.html.twig', ['tripForm'=>$tripForm->createView(),
         ]);
     }
 
 
-    // A voir si on ne met pas tous les updates des status dans un SERVICE ?
+
+
+
 
     /**
-     * @Route("/update/{id}/{stateId}", name="update")
+     * @Route("/edit/{id}", name="edit")
      */
 
-    public function updateTripState(int $id, int $stateId, TripRepository $tripRepository)
-    {
-        return $this->render('trip/edit.html.twig', ['tripRepo'=>$tripRepository->updateState($id, $stateId)]);
-
-
+    public function edit( int $id, Request $request,EntityManagerInterface $em) : Response {
+        $trip = $em->getRepository(Trip::class)->find($id);
+        if (!$trip) {
+            throw $this->createNotFoundException("Cette sortie est inconnue");
         }
-
-    /**
-     * @throws \Exception
-     */
-    public function updateAllStateTrip(TripRepository $tripRepository){
-
-        $today=new \DateTime('now');
-
-        $trips = $tripRepository->findAllTrip();
-        foreach ($trips as $trip){
-            if ($trip->getState() == 2 && $trip->getLimitDate() == $today ){
-                $tripRepository ->updateState($trip->getId(), 3);
-            }elseif ($trip->getState()==3 && $trip->getStartHour() == $today){
-                $tripRepository ->updateState($trip->getId(), 4);
-            }elseif($trip->getState()==4 && $trip->getStartHour()->add(new \DateInterval('PT'.$trip->getDuration()->timestamp().'S')) > $today){
-                $tripRepository ->updateState($trip->getId(), 5);
-            }elseif($trip->getState()>=5 && $today > $trip->getStartHour()->add(new \DateInterval('PT1M'))){
-                $tripRepository ->updateState($trip->getId(), 7);
-
+        $tripForm = $this->createForm(TripType::class, $trip);
+        $tripForm->handleRequest($request);
+        if ($tripForm->isSubmitted() && $tripForm->isValid()) {
+            $em->persist($trip);
+            $em->flush();
+            $this->addFlash("success", "La sortie est bien modifiée.");
+            return $this->redirectToRoute("trip_display", ["id"=> $trip->getId()]);
         }
+        return $this->render('trip/edit.html.twig', [
+            'tripForm'=>$tripForm->createView(),
+        ]);
 
 
     }
-
-  /*  public function updateStateTripArchived(TripRepository $tripRepository, EntityManagerInterface $entityManager){
-        $entityManager=$this->getDoctrine()->getManager();
-        $trip = $entityManager ->getRepository(Trip::class)->findTripFinishedAndCancel();
-        if ()
-    }*/
-}}
-    
-    
-  
-  
+}
+        
 
