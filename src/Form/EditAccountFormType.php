dcea905a4577b1b227986811424e0080a7d5603c
Dorian <dorian.guler2021@campus-eni.fr>
2021-07-22 09:12:49 +0200

    Commit mise en commentaire de la partie Photo de profil qui n'est pas fini

--- src/Form/EditAccountFormType.php
+++ src/Form/EditAccountFormType.php
@@ -6,11 +6,13 @@ use App\Entity\User;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Image;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
@@ -63,6 +65,11 @@ class EditAccountFormType extends AbstractType
                 'choice_label' => 'name',
                 'required' => false,
             ])
+            //->add('picture', FileType::class, [
+            //    'label' => 'Photo de profil',
+            //    'mapped' => false,
+            //    'required' => false,
+            //])
         ;
     }
 
